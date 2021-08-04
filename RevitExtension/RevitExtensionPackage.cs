da643d14481ddb474113687fefe41f15fc4818ee
Leandro Teles <lteles@thebimdev.com>
2021-08-03 17:48:53 -0700

    Added GNU license.

--- RevitExtension/RevitExtensionPackage.cs
+++ RevitExtension/RevitExtensionPackage.cs
@@ -1,4 +1,17 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿//    The BIMDev Extension is an extension for Visual Studio that provides a template for creating a Revit Addin.
+//    Copyright(C) 2021  BIMDev LLC
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
--- RevitExtension/Wizard.cs
+++ RevitExtension/Wizard.cs
@@ -1,4 +1,17 @@
-﻿using EnvDTE;
+﻿//    The BIMDev Extension is an extension for Visual Studio that provides a template for creating a Revit Addin.
+//    Copyright(C) 2021  BIMDev LLC
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 using System;
 using System.Collections.Generic;
