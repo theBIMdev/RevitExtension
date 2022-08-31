f875c5f620bd8ad5311d11c2b86d0a37934a451b
Leandro Teles <lteles@thebimdev.com>
2022-08-31 03:00:55 -0700

    Initial functionality achieved

---
+++ RevitExtension/ToolWindows/AddinEntry.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RevitExtension.RevitAddinControl;
+
+namespace RevitExtension;
+
+public class AddinEntry : INotifyPropertyChanged
+{
+    private ObservableCollection<AddinEntry> _children = new();
+    private bool? _onByDefault;
+    private bool? _onByDebug;
+
+    public AddinEntry() { }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        handler?.Invoke(this, e);
+    }
+    public string AddinName { get; set; }
+    public string AddinYear { get; set; }
+    public string DisplayName { get; set; }
+    public bool? OnByDefault
+    {
+        get => _onByDefault;
+        set
+        {
+            _onByDefault = value;
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(OnByDefault)));
+        }
+    }
+    public string OnByDefaultToolTip { get; set; }
+    public bool? OnByDebug
+    {
+        get => _onByDebug;
+        set
+        {
+            _onByDebug = value;
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(OnByDebug)));
+        }
+    }
+    public string OnByDebugToolTip { get; set; }
+    public bool? IsEnabled { get; set; }
+    public string FilePath { get; set; }
+    public bool IsUpdating { get; set; } = false;
+
+    public AddinEntry Parent { get; set; }
+    public string MarginOffset { get => Parent is null ? "0, 0, 0, 0" : "-38, 0, 0, 0"; }
+    public string Opacity { get => Parent is null ? "1.0" : "0.75"; }
+    public ObservableCollection<AddinEntry> Children
+    {
+        get => _children;
+        set
+        {
+            _children = value;
+            foreach (var child in _children) { child.Parent = this; }
+        }
+    }
+
+    public void UpdateChildren(string propertyName)
+    {
+        if (propertyName == nameof(OnByDefault))
+        {
+            foreach (var child in _children.Where(child => child.OnByDefault != OnByDefault))
+            {
+                child.OnByDefault = OnByDefault;
+            }
+        }
+        else if (propertyName == nameof(OnByDebug))
+        {
+            foreach (var child in _children.Where(child => child.OnByDebug != OnByDebug))
+            {
+                child.OnByDebug = OnByDebug;
+            }
+        }
+    }
+
+    public void VerifyCheckState(string propertyName)
+    {
+        if (!_children.Any()) { return; }
+
+        if (propertyName == nameof(OnByDefault))
+        {
+            bool? byDefault = _children.First().OnByDefault;
+            foreach (var child in _children)
+            {
+                if (child.OnByDefault != byDefault)
+                { byDefault = null; break; }
+            }
+            OnByDefault = byDefault;
+        }
+
+        else if (propertyName == nameof(OnByDebug))
+        {
+            bool? byDebug = _children.First().OnByDebug;
+            foreach (var child in _children)
+            {
+                if (child.OnByDebug != byDebug)
+                { byDebug = null; break; }
+            }
+            OnByDebug = byDebug;
+        }
+    }
+
+    public void MatchPropertySilently(string propertyName, AddinEntry entry)
+    {
+        FilePath = entry.FilePath;
+        if (propertyName == nameof(OnByDefault))
+        {
+            _onByDefault = entry.OnByDefault;
+        }
+        else if (propertyName == nameof(OnByDebug))
+        {
+            _onByDebug = entry.OnByDebug;
+        }
+    }
+
+    public void UpdateFile(DebugState debugger)
+    {
+        if (FilePath is null) return;
+
+        switch (debugger)
+        {
+            case DebugState.None:
+                if (!OnByDefault.HasValue) return;
+                if (OnByDefault is true) { turnOn(FilePath); }
+                else { turnOff(FilePath); }
+                break;
+            case DebugState.Starting:
+                break;
+            case DebugState.Ending:
+                break;
+        }
+    }
+
+    private void turnOn(string file)
+    {
+        string newName = file.Replace(".addin.off", ".addin");
+        File.Move(file, newName);
+        FilePath = newName;
+    }
+    private void turnOff(string file)
+    {
+        string newName = file.Replace(".addin", ".addin.off");
+        File.Move(file, newName);
+        FilePath = newName;
+    }
+}
+
--- RevitExtension/ToolWindows/RevitAddinControl.xaml.cs
+++ RevitExtension/ToolWindows/RevitAddinControl.xaml.cs
@@ -1,14 +1,167 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace RevitExtension;
 public partial class RevitAddinControl : UserControl
 {
+    public enum DebugState { None, Starting, Ending }
+    public ObservableCollection<AddinEntry> EntriesByAddin { get; set; } = new();
+    public ObservableCollection<AddinEntry> EntriesByYear { get; set; } = new();
     public ManifestLocations Locations { get; } = new();
     public ToolTips ToolTips { get; } = new();
+    
+    private const string _changeGroupingToYear = "Group by Revit Year";
+    private const string _changeGroupingToName = "Group by Addin Name";
+
     public RevitAddinControl()
     {
         InitializeComponent();
+        Locations.PropertyChanged += new PropertyChangedEventHandler(location_PropertyChanged);
+        refreshList(Locations.MachinePath, Locations.UserPath);
+    }
+
+    private void refreshList(string machinePath, string userPath)
+    {
+        getAddinEntries(machinePath, userPath);
+
+        trvMenu.Items.Clear();
+        if (groupingToggle.Content is null || groupingToggle.Content.ToString() == _changeGroupingToYear)
+        { foreach (var entry in EntriesByAddin) { trvMenu.Items.Add(entry); } }
+        else
+        { foreach (var entry in EntriesByYear) { trvMenu.Items.Add(entry); } }
+    }
+
+    private void getAddinEntries(string machinePath, string userPath)
+    {
+        SortedDictionary<string, ObservableCollection<AddinEntry>> byYear = new();
+        SortedDictionary<string, ObservableCollection<AddinEntry>> byAddin = new();
+
+        var directories = Directory.GetDirectories(machinePath).ToList();
+        directories.AddRange(Directory.GetDirectories(userPath));
+
+        foreach (string path in directories)
+        {
+            string year = path.Substring(path.Length - 4);
+            if (!byYear.ContainsKey(year)) { byYear.Add(year, new()); }
+
+            var files = Directory.GetFiles(path).Where(p => p.EndsWith(".addin") || p.EndsWith(".off"));
+            foreach (string filePath in files)
+            {
+                int offset = filePath.EndsWith(".off") ? 10 : 6;
+                string addinName = Path.GetFileName(filePath);
+                addinName = addinName.Substring(0, addinName.Length - offset);
+                if (!byAddin.ContainsKey(addinName)) { byAddin.Add(addinName, new()); }
+
+                bool onByDefault = filePath.EndsWith(".addin");
+                bool onByDebug = false; // TODO: Use a config file.
+
+                var yearEntry = new AddinEntry()
+                {
+                    DisplayName = addinName,
+                    AddinName = addinName,
+                    AddinYear = year,
+                    OnByDefault = onByDefault,
+                    OnByDebug = onByDebug,
+                    FilePath = filePath
+                };
+                yearEntry.PropertyChanged += new PropertyChangedEventHandler(childEntry_PropertyChanged);
+                byYear[year].Add(yearEntry);
+
+                var nameEntry = new AddinEntry()
+                {
+                    DisplayName = year,
+                    AddinName = addinName,
+                    AddinYear = year,
+                    OnByDefault = onByDefault,
+                    OnByDebug = onByDebug,
+                    FilePath = filePath
+                };
+                nameEntry.PropertyChanged += new PropertyChangedEventHandler(childEntry_PropertyChanged);
+                byAddin[addinName].Add(nameEntry);
+            }
+        }
+
+        EntriesByYear.Clear();
+        foreach (var kvp in byYear)
+        {
+            var parent = new AddinEntry()
+            {
+                DisplayName = kvp.Key,
+                Children = kvp.Value
+            };
+            parent.VerifyCheckState(nameof(parent.OnByDefault));
+            parent.VerifyCheckState(nameof(parent.OnByDebug));
+            parent.PropertyChanged += new PropertyChangedEventHandler(parentEntry_PropertyChanged);
+            EntriesByYear.Add(parent);
+        }
+
+        EntriesByAddin.Clear();
+        foreach (var kvp in byAddin)
+        {
+            var parent = new AddinEntry()
+            {
+                DisplayName = kvp.Key,
+                Children = kvp.Value
+            };
+            parent.VerifyCheckState(nameof(parent.OnByDefault));
+            parent.VerifyCheckState(nameof(parent.OnByDebug));
+            parent.PropertyChanged += new PropertyChangedEventHandler(parentEntry_PropertyChanged);
+            EntriesByAddin.Add(parent);
+        }
+    }
+
+    private void parentEntry_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (sender is AddinEntry parent && !parent.IsUpdating)
+        {
+            parent.IsUpdating = true;
+            parent.UpdateChildren(e.PropertyName);
+            parent.IsUpdating = false;
+        }
+    }
+    private void childEntry_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (sender is AddinEntry child && !child.IsUpdating)
+        {
+            child.IsUpdating = true;
+            var entryByYear = EntriesByYear.FirstOrDefault(item => item.DisplayName == child.AddinYear)?
+                .Children.FirstOrDefault(item => item.DisplayName == child.AddinName);
+            var entryByAddin = EntriesByAddin.FirstOrDefault(item => item.DisplayName == child.AddinName)?
+                .Children.FirstOrDefault(item => item.DisplayName == child.AddinYear);
+
+            entryByYear?.MatchPropertySilently(e.PropertyName, child);
+            entryByAddin?.MatchPropertySilently(e.PropertyName, child);
+
+            if (child.Parent is AddinEntry parent && !parent.IsUpdating)
+            {
+                parent.PropertyChanged -= new PropertyChangedEventHandler(parentEntry_PropertyChanged);
+                parent.VerifyCheckState(e.PropertyName);
+                parent.PropertyChanged += new PropertyChangedEventHandler(parentEntry_PropertyChanged);
+            }
+
+            if (e.PropertyName == nameof(child.OnByDefault))
+            {
+                child.UpdateFile(DebugState.None);
+            }
+            else if (e.PropertyName == nameof(child.OnByDebug))
+            {
+                //TODO: Update config file
+            }
+            child.IsUpdating = false;
+        }
+
+    }
+    private void location_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (sender is ManifestLocations locations)
+        {
+            refreshList(locations.MachinePath, locations.UserPath);
+        }
     }
 
     private void btn_RevitLocation_Click(object sender, RoutedEventArgs e)
@@ -32,6 +185,36 @@ public partial class RevitAddinControl : UserControl
         if (dialog.ShowDialog().GetValueOrDefault())
         {
             Locations.UserPath = dialog.SelectedPath;
+        }
+    }
+
+    private void groupingToggle_Click(object sender, RoutedEventArgs e)
+    {
+        if (trvMenu is not null)
+        {
+            trvMenu.Items.Clear();
+            if (groupingToggle.Content.ToString() == _changeGroupingToName)
+            {
+                groupingToggle.Content = _changeGroupingToYear;
+                foreach (var entry in EntriesByAddin) { trvMenu.Items.Add(entry); } 
+            }
+            else
+            {
+                groupingToggle.Content = _changeGroupingToName;
+                foreach (var entry in EntriesByYear) { trvMenu.Items.Add(entry); } 
+            }
+        }
+    }
+
+    private void textBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+
+        if (e.ClickCount == 2 && 
+            sender is TextBlock textBlock && 
+            textBlock.ToolTip is string path &&
+            File.Exists(path))
+        {
+            System.Diagnostics.Process.Start(path);
         }
     }
 }
